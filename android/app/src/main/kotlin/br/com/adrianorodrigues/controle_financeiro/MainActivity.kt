04d995fb0f9d90a30fc8635f32bc47e70b5fdd89
Adriano <adrianomrodrigues93@gmail.com>
2019-10-16 00:53:19 -0300

    projeto inicial

---
+++ android/app/src/main/kotlin/br/com/adrianorodrigues/controle_financeiro/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.adrianorodrigues.controle_financeiro
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
