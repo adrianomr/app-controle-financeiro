9acbefbe71c9aa40256362d56fde97e06b55dfc9
Adriano <adrianomrodrigues93@gmail.com>
2019-10-18 22:24:20 -0300

    adicionando lista de compras

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  Screen2State createState() => Screen2State();
+}
+
+class Screen2State extends State<Screen2> {
+  List itens = new List();
+  String item = "";
+
+  @override
+  void initState() {
+    super.initState();
+    itens.add("Teste 1");
+    itens.add("Teste 2");
+    itens.add("Teste 3");
+    itens.add("Teste 4");
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -13,34 +29,44 @@ class Screen2 extends StatelessWidget {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('Teste'),
+        title: Text('Market List'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            itens.add(item);
+            item = "";
+            print("added item -> " + item);
+          });
+        },
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World',
-            ),
-          ],
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, hintText: 'Add new item'),
+          onChanged: (item){
+            setState(() {
+              this.item = item;
+            });
+          },
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Expanded(
+          child: SizedBox(
+              height: 200.0,
+              child: ListView.builder(
+            itemCount: itens.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Dismissible(
+                key: Key(index.toString()),
+                child: Text(itens[index]),
+              );
+            }),
+          )
+      )
+      ])), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
