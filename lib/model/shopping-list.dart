3a727721718ebbc9cb1f929d9dcdec3447690496
Adriano <adrianomrodrigues93@gmail.com>
2019-11-05 21:47:03 -0300

    Tornando shopping-list um singleton

--- lib/model/shopping-list.dart
+++ lib/model/shopping-list.dart
@@ -3,7 +3,14 @@ import 'package:controle_financeiro/model/product.dart';
 import 'base/shopping-list-base.dart';
 
 class ShoppingList extends ShoppingListBase {
-  ShoppingList({
-    List<Product> product,
-  }) : super(product: product);
+
+  static ShoppingList _shoppingList;
+
+  factory ShoppingList({List<Product> product}) {
+    _shoppingList ??= ShoppingList._internalConstructor(product: product);
+    return _shoppingList;
+  }
+
+  ShoppingList._internalConstructor({List<Product> product})
+      : super(product: product);
 }
