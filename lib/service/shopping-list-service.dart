1c5b4185e4326c15e98ec9a56c735ae24841b4c3
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2020-05-30 20:15:25 -0300

    Criando app para controle de acoes

--- lib/service/shopping-list-service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-
-Future<String> getShoppingList() async {
-  final response = await http
-      .get('https://ar-controle-financeiro.herokuapp.com/shopping-list');
-  print('shopping itens:' + response.body);
-  return response.body;
-}
